068955b558e0c0a49ae8dabc84002e6772ff3788
yvyletkov <yvyletkov@ya.ru>
2021-01-24 01:14:00 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    window.addEventListener('scroll', () => {
+
+        const anchorsEl =  document.querySelector('.anchors-wrapper')
+        if (window.pageYOffset > (window.innerHeight - 30)) anchorsEl.classList.add('fixed')
+        else anchorsEl.classList.remove('fixed')
+
+    })
+}
+
+const openForm = () => {
+    document.querySelector('.form-wrapper').classList.add('active')
+    document.querySelector('.text').classList.add('hidden')
+    document.querySelector('.order-btn').classList.add('active')
+}
+
+const closeForm = () => {
+    document.querySelector('.form-wrapper').classList.remove('active')
+    document.querySelector('.order-btn').classList.remove('active')
+    document.querySelector('.text').classList.remove('hidden')
+
+
+}

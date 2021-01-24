fa16594bc6f952933e5dc70834484d405dc316c8
yvyletkov <yvyletkov@ya.ru>
2021-01-24 15:42:44 +0300

    add 2 works & 2 form fields

--- scripts.js
+++ scripts.js
@@ -6,6 +6,10 @@ window.onload = () => {
         else anchorsEl.classList.remove('fixed')
 
     })
+
+    document.querySelectorAll('.form input').forEach(el => el.addEventListener('change', (e) => {
+        e.target.nextSibling.nextElementSibling.classList.add('minified')
+    }))
 }
 
 const openForm = () => {
@@ -18,6 +22,4 @@ const closeForm = () => {
     document.querySelector('.form-wrapper').classList.remove('active')
     document.querySelector('.order-btn').classList.remove('active')
     document.querySelector('.text').classList.remove('hidden')
-
-
 }

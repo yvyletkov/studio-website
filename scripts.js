007df668e6338ce128389f86bda7491e114d9d1e
yvyletkov <yvyletkov@ya.ru>
2021-01-25 14:59:41 +0300

    bem refactoring

--- scripts.js
+++ scripts.js
@@ -13,13 +13,39 @@ window.onload = () => {
 }
 
 const openForm = () => {
-    document.querySelector('.form-wrapper').classList.add('active')
-    document.querySelector('.text').classList.add('hidden')
+    document.querySelector('.form-block').classList.add('active')
+    document.querySelector('.home__container__text').classList.add('hidden')
     document.querySelector('.order-btn').classList.add('active')
 }
 
 const closeForm = () => {
-    document.querySelector('.form-wrapper').classList.remove('active')
+    document.querySelector('.form-block').classList.remove('active')
+    document.querySelector('.home__container__text').classList.remove('hidden')
     document.querySelector('.order-btn').classList.remove('active')
-    document.querySelector('.text').classList.remove('hidden')
+}
+
+function fade(element) {
+    let op = 1;
+    let timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function unfade(element) {
+    let op = 0.1;
+    element.style.display = 'block';
+    let timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
 }
